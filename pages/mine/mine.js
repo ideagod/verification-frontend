5d9765a202eb747a19088fd385ff980099348a13
赖幸儿 <laixe@breo.com.cn>
2022-12-23 14:39:34 +0800

    feat: 摄像头demo编写

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,13 +1,51 @@
 Page({
-  data: {
-    show: false,
-  },
+    data: {
+        show: false,
+        type: 0,
+        typeDetail: [{
+            title:'登 录',
+            text: '工号',
+            field: 'account',
+        },{
+            title:'注 册',
+            text: '姓名',
+            field: 'name',
+        }],
+        account:'',
+        name:''
+    },
+    changeType(e){
+        this.setData({
+            type: e.target.dataset.type
+        });
+    },
 
-  showPopup() {
-    this.setData({ show: true });
-  },
+    showPopup() {
+        this.setData({
+            show: true
+        });
+    },
 
-  onClose() {
-    this.setData({ show: false });
-  },
+    onClose() {
+        this.setData({
+            show: false,
+            type: 0
+        });
+    },
+    getAccount(event) {
+        this.setData({
+            account: event.detail.value
+        })
+    },
+    getName(event) {
+        this.setData({
+            name: event.detail.value
+        })
+    },
+    navToVerify() {
+        console.log(this.data.name, this.data.account,this.data.type)
+        wx.navigateTo({
+            url: '/pages/camera/camera'
+        })
+    }
 });
